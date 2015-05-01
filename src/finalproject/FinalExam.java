016e737f3df72d30921929ad985741b186d54494
abreun68 <nasser.abreu@gmail.com>
2015-04-30 22:27:44 -0400

    cleanup

--- src/finalproject/FinalExam.java
+++ src/finalproject/FinalExam.java
@@ -21,14 +21,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 /**
- * This program will connect to a website that has a folder which contains a list of images.
- * The images will then be downloaded to our local computer and then altered, by changing
- * it to a black and white .jpg file.  The file will then be written to the hard drive.
+ * This program will connect to a website that has a folder which contains a
+ * list of images. The images will then be downloaded to our local computer and
+ * then altered, by changing it to a black and white .jpg file. The file will
+ * then be written to the hard drive.
  * <p>
- * The purpose of this program is to create a true concurrent solution that will begin
- * altering the images while the other files are still downloading and will perhaps
- * even begin writing the images to the hard drive while the downloads are still processing.
- * This is an example of a true producer consumer problem.
+ * The purpose of this program is to create a true concurrent solution that will
+ * begin altering the images while the other files are still downloading and
+ * will perhaps even begin writing the images to the hard drive while the
+ * downloads are still processing. This is an example of a true producer
+ * consumer problem.
+ *
  * @author Nacer Abreu
  * @author Michael Norris
  * @author Emmanuel Bonilla
@@ -37,60 +40,52 @@ import org.jsoup.nodes.Element;
 public class FinalExam {
 
     private static final String URL_PATH = "http://elvis.rowan.edu/~mckeep82/ccpsp15/Astronomy/";
-    private static ExecutorService exec;
-    private static Collection<Callable<String>> callables;
-    private static BlockingQueue<String> completedDownloads;
-    private static ArrayList<String> fileNameList;
-    private static HashMap<String, Future> futures;
-    
+
     public static void main(String[] args) throws Exception {
-    	
+
         //****************************
         // Get the list of the file names
         //****************************
-        fileNameList = buildUrlList();
-        
-        callables = new ArrayList<>(); 
-        
-    	// Delete all JPG files
-    	DeleteJpgFiles.delJpg();
-	 
+        ArrayList<String> fileNameList = buildUrlList();
+
+        // Delete all JPG files
+        DeleteJpgFiles.delJpg();
 
         // Determines the number of threads to use by finding out how many 
         // processors are available and then adding 1
         int NTHREADS = Runtime.getRuntime().availableProcessors() + 1;
-        exec = Executors.newFixedThreadPool(NTHREADS);
+        ExecutorService exec = Executors.newFixedThreadPool(NTHREADS);
 
-        
         // BlockingQueue of completed downloads. 
-        completedDownloads = new ArrayBlockingQueue<>(fileNameList.size());
-        
+        BlockingQueue<String> completedDownloads = new ArrayBlockingQueue<>(fileNameList.size());
+
         // This hashmap is used by the consumer thread to verify that the file
         // that is going to be altered has finished downloading. That the task
         // is done. 
-        futures = new HashMap<>();
-        
+        HashMap<String, Future> futures = new HashMap<>();
 
-         callables.add(getConsumerCallable());
-        
-         callables.add(getProducerCallable());
+        Collection<Callable<String>> callables = new ArrayList<>();
+
+        callables.add(getConsumerCallable(fileNameList, completedDownloads, futures, exec));
+
+        callables.add(getProducerCallable(fileNameList, completedDownloads, futures, exec));
 
         // invokeAll is a blocking method. It means – JVM won’t proceed to next 
         // line until the consumer and producer threads are done.
         exec.invokeAll(callables);
-        
+
         // Waits until all tasks are completed before graciously shuting down
         // the executor
         exec.shutdown();
 
     }//end of main()
-    
+
     /**
-     * This method connects to the given URL path and reads the code
-     * to find and build a list of .jpg files.  The code is read and searches
-     * for lines that contain .jpg extension.  The line is then sent to
-     * the parseHtml method to isolate and return the filename so it can 
-     * be added to our urlList.
+     * This method connects to the given URL path and reads the code to find and
+     * build a list of .jpg files. The code is read and searches for lines that
+     * contain .jpg extension. The line is then sent to the parseHtml method to
+     * isolate and return the filename so it can be added to our urlList.
+     *
      * @return urlList - returns a list of .jpg files
      */
     private static ArrayList<String> buildUrlList() {
@@ -98,18 +93,18 @@ public class FinalExam {
 
         try {
             //Sets up the URL PATH
-        	URL url = new URL(URL_PATH);
-            
+            URL url = new URL(URL_PATH);
+
             //Returns a URLConnection instance that represents a connection to the remote object referred to by the URL
             URLConnection conn = url.openConnection();
-            
+
             InputStream inputStream = conn.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line = null;
             while ((line = reader.readLine()) != null) {
                 if (line.contains(".jpg")) {
                     //System.out.println(line);
-                	//Calls parseHtml method to isolate the name of the .jpg file
+                    //Calls parseHtml method to isolate the name of the .jpg file
                     String fileName = parseHtml(line);
                     //Adds the name of the .jpg file to our urlList
                     urlList.add(fileName);
@@ -123,10 +118,11 @@ public class FinalExam {
     } //end buildUrlList() method
 
     /**
-     * This method returns the name of .jpg that will be processed later
-     * by parsing through the line of code that is given to this method.
-     * This method must be given a String that contains the HTML tags
+     * This method returns the name of .jpg that will be processed later by
+     * parsing through the line of code that is given to this method. This
+     * method must be given a String that contains the HTML tags
      * <a href="somefilename.jpg">...</a>
+     *
      * @param line - the line of code that contains the name of the .jpg file
      * @return fileName - returns the name of the .jpg file
      */
@@ -134,7 +130,7 @@ public class FinalExam {
 
         //Parses the line into a new Document so we can use the appropriate methods in Document to access certain data 
         Document doc = Jsoup.parse(line);
-        
+
         // finds the first link with "a" tags (contains href attributes)
         Element link = doc.select("a").first();
 
@@ -145,26 +141,28 @@ public class FinalExam {
     } //end parseHtml() method
 
     /**
-     * 
+     *
      */
-    private static Callable getConsumerCallable() throws Exception {
+    private static Callable<String> getConsumerCallable(ArrayList<String> fileNameList,
+            BlockingQueue<String> completedDownloads, HashMap<String, Future> futures, ExecutorService exec) throws Exception {
         return ((Callable) () -> {
             System.out.println("Consumer waiting...");
             for (String loop : fileNameList) {
-                
+
                 String fileName = completedDownloads.take();
                 exec.execute(new AlterImageTask(fileName, futures));
-                
+
             }//end of for..loop
             return null;
         });
     }//end of getConsumerCallable()
 
     /**
-     * 
+     *
      */
-    private static Callable getProducerCallable(){
-        return(new Callable() {
+    private static Callable<String> getProducerCallable(ArrayList<String> fileNameList,
+            BlockingQueue<String> completedDownloads, HashMap<String, Future> futures, ExecutorService exec) {
+        return (new Callable() {
 
             @Override
             public Object call() throws Exception {
@@ -174,7 +172,7 @@ public class FinalExam {
                 }
                 return null;
             }
-        });        
+        });
     }//end of getProducerCallable();
-    
+
 }//end of class
