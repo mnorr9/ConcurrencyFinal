e2901a78f9520792f61cefeec3f7668bc2bf2b71
abreun68 <nasser.abreu@gmail.com>
2015-04-30 22:28:33 -0400

    cleanup

--- src/finalproject/FinalExam.java
+++ src/finalproject/FinalExam.java
@@ -41,6 +41,11 @@ public class FinalExam {
 
     private static final String URL_PATH = "http://elvis.rowan.edu/~mckeep82/ccpsp15/Astronomy/";
 
+    /**
+     * Entry point of this program.
+     * @param args
+     * @throws Exception 
+     */
     public static void main(String[] args) throws Exception {
 
         //****************************
