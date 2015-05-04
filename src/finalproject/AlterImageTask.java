637f95f39ec7ba96d8da956937d8c1b85d5daac0
ebonilla <ebonilla@MSE-J7R7BW1.njmrsnet-aegis.com>
2015-05-03 22:40:05 -0400

    Added comments

--- src/finalproject/AlterImageTask.java
+++ src/finalproject/AlterImageTask.java
@@ -13,19 +13,27 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 /**
- *
- *
+ * This class is used to alter an image to black and white
+ * 
+ * @author Nacer Abreu
+ * @author Michael Norris
+ * @author Emmanuel Bonilla
  */
 public class AlterImageTask implements Runnable {
 
     private final String fileName;
     private final Future future;
 
+    /**
+     * Constructor for AlterImageTask class
+     * @param fileName - name of file to be processed
+     * @param future - name of future to be processed 
+     */
     public AlterImageTask(String fileName, Future future) {
 
         this.future = future;
         this.fileName = fileName;
-    }
+    } //end AlterImageTask()
 
     @Override
     public void run() {
@@ -43,9 +51,11 @@ public class AlterImageTask implements Runnable {
                     img.getWidth(), img.getHeight(),
                     BufferedImage.TYPE_BYTE_BINARY);
 
+            //creating a Graphics2D object to be processed
             Graphics2D graphics = blackAndWhiteImg.createGraphics();
             graphics.drawImage(img, 0, 0, null);
             String bwFileName = "bw_" + fileName;
+            //writes an image in the given format
             ImageIO.write(blackAndWhiteImg, "jpg", new File(bwFileName));
             System.out.println("Altering : " + bwFileName + "; size: " + getSize(fileName) + "kb");
         } catch (IOException ex) {
@@ -55,6 +65,11 @@ public class AlterImageTask implements Runnable {
 
     }//end of run();
 
+    /**
+     * This method will take a filename and return the size of the file in kilobytes
+     * @param filename - name of file
+     * @return kilobytes - size of file in kilobytes
+     */
     private long getSize(String filename) {
         File file = new File("bw_"+filename);
         long kilobytes = 0;
@@ -64,6 +79,6 @@ public class AlterImageTask implements Runnable {
             kilobytes = (bytes / 1024);
         }
         return kilobytes;
-    }
+    } //end getSize()
     
 }//end of class()
