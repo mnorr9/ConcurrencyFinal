f3b36529a836e356272716422bed46fde830f6e9
abreun68 <nasser.abreu@gmail.com>
2015-05-05 23:35:09 -0400

    More cleanup

--- src/finalproject/DownloadImageTask.java
+++ src/finalproject/DownloadImageTask.java
@@ -1,4 +1,3 @@
-
 package finalproject;
 
 import java.io.File;
@@ -6,14 +5,17 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * This class is used to implement the downloading of files from the given website
- * 
+ * This class is used to implement the downloading of files from the given
+ * website
+ *
  * @author Nacer Abreu
  * @author Michael Norris
  * @author Emmanuel Bonilla
- * 
+ *
  */
 public class DownloadImageTask implements Runnable {
 
@@ -23,62 +25,67 @@ public class DownloadImageTask implements Runnable {
 
     /**
      * Constructor for filename to be downloaded
+     *
      * @param fileName - name of file to be downloaded
      */
     public DownloadImageTask(String fileName) {
         this.fileName = fileName;
 
     }
-    
+
     /**
      * Returns the name of file
+     *
      * @return - returns the name of the file
      */
-    public String getFilename(){
+    public String getFilename() {
         return fileName;
     }
 
     @Override
     public void run() {
     	//****************************
-    	// Download all images
-    	//****************************
-    	try {
+        // Download all images
+        //****************************
+        try {
 
-    		//sets up the path to which we will be downloading
-    		URL	url = new URL(URL_PATH+fileName);
+            //sets up the path to which we will be downloading
+            URL url = new URL(URL_PATH + fileName);
 
-    		//sets up the input and output streams for retrieving and saving the file to be downloaded
-    		InputStream is = url.openStream();
-    		OutputStream os = new FileOutputStream(fileName);
+            //sets up the input and output streams for retrieving and saving the file to be downloaded
+            InputStream is = url.openStream();
+            OutputStream os = new FileOutputStream(fileName);
 
-    		byte[] b = new byte[2048];
-    		int length;
-                
-    		while ((length = is.read(b)) != -1) {
-    			os.write(b, 0, length);
-    		}
+            byte[] b = new byte[2048];
+            int length;
 
-    		is.close();
-    		os.close();
+            while ((length = is.read(b)) != -1) {
+                os.write(b, 0, length);
+            }
 
-    		System.out.println("Downloaded : "+fileName + "; size: " + getSize(fileName) + "kb");
-    	} catch (Exception ex) {
-    		ex.printStackTrace();
-    	}
+            is.close();
+            os.close();
+
+            System.out.println("Downloaded : " + fileName + "; size: " + getSize(fileName) + "kb");
+        } catch (Exception ex) {
+            System.err.println("Filename: " + fileName);
+            Logger.getLogger(DownloadImageTask.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
-    
+
     /**
-     * This method will take a filename and return the size of the file in kilobytes
+     * This method will take a filename and return the size of the file in
+     * kilobytes
+     *
      * @param filename - name of file to be processed
      * @return kilobytes - returns the size of the file in kilobytes
      */
-    private long getSize(String filename){
-        File file =new File(filename);
+    private long getSize(String filename) {
+        File file = new File(filename);
         long kilobytes = 0;
-        
-        if(file.exists()){
+
+        if (file.exists()) {
             long bytes = file.length();
             kilobytes = (bytes / 1024);
         }
